682b39a60b781783a214f5023b5be790731b9f05
suiwave <184898096+suiwave@users.noreply.github.com>
2024-12-28 11:12:48 +0900

    lint 対応

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+// biome-ignore lint/style/noNonNullAssertion: non-nullが保障されているため
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
